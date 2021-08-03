d41f8456e70cd50ce8f299bd533d84390e6c1bbb
arslannmrv <48587016+arslannmrv@users.noreply.github.com>
2021-08-03 12:37:56 +0300

    Update post_cubit.dart

--- lib/bloc/post_cubit.dart
+++ lib/bloc/post_cubit.dart
@@ -5,14 +5,14 @@ import 'post_repository.dart';
 
 
 class PostsCubit extends Cubit<PostsState> {
-  final PostsRepository _catsRepository;
-  PostsCubit(this._catsRepository) : super(PostsInitial());
+  final PostsRepository _postsRepository;
+  PostsCubit(this._postsRepository) : super(PostsInitial());
 
   Future<void> getPosts() async {
     try {
       emit(PostsLoading());
       await Future.delayed(Duration(milliseconds: 500));
-      final response = await _catsRepository.getPosts();
+      final response = await _postsRepository.getPosts();
       emit(PostsCompleted(response));
     } on NetworkError catch (e) {
       emit(CatsError(e.message));
